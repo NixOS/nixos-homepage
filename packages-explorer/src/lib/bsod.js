534810dc3686ccaf64941ead8f01a61410c5baa0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-08-20 09:26:18 -0400

    packages-explorer: reviews fatal error message

--- packages-explorer/src/lib/bsod.js
+++ packages-explorer/src/lib/bsod.js
@@ -5,15 +5,16 @@
  */
 const bsod = function(msg = "Something happened.") {
 	const el = document.getElementById("packages-explorer");
-	el.innerText =
+	const newEl = document.createElement("div");
+	newEl.innerHTML =
 `Something went wrong...
 
 -> ${msg}
 
-Please contact us with details stating what you were doing
-and the full message, we may be able to resolve the situation.
-`;
-	el.className = "bsod";
+Please <a href="https://github.com/NixOS/nixos-homepage/issues/new">report a bug</a> with details stating what you were doing and the full message, we may be able to resolve the situation.`;
+	el.parentNode.replaceChild(newEl, el);
+	newEl.className = "bsod";
 };
 
+window.bsod = bsod;
 export default bsod;
