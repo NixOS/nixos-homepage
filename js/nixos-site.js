582ecef8f0582eac50cb3b16aff3b7a294c72b2c
Rok Garbas <rok@garbas.si>
2020-05-13 20:43:39 +0200

    simplify landing page

--- js/nixos-site.js
+++ js/nixos-site.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $(".nixos-popover").popover({});
   $(".navbar-search a").click(function (event) {
+    event.preventDefault();
     var url = window.location.protocol + "//" + window.location.host + "/nixos";
     if ($(this).text() === "Options") {
       url += "/options.html";
@@ -11,7 +12,13 @@ $(document).ready(function () {
       url += "&query=" + $("input", $(this).parents("form")).val();
     }
     window.location.href = url;
+  });
+  $("#packages-search button").click(function (event) {
     event.preventDefault();
+    var url = window.location.protocol + "//" + window.location.host + "/nixos/packages.html"
+    url += "?channel=nixos-" + $(document.body).data('latest-nixos-series');
+    url += "&query=" + $("input", $(this).parents("form")).val();
+    window.location.href = url;
   });
   var qs = new URLSearchParams(window.location.search.substring(1));
   var query = qs.get("query");
