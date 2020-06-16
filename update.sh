d2cff19b1a25e6580e880bdaa2ba72a0b51d53ea
Rok Garbas <rok@garbas.si>
2020-06-16 09:14:29 +0200

    automatically update released-nix input

--- update.sh
+++ update.sh
@@ -6,4 +6,5 @@ UPDATE=1 nix shell nixpkgs#gnumake nixpkgs#curl -c make update --keep-going
 
 nix flake update \
   --update-input released-nixpkgs \
+  --update-input released-nix \
   --update-input nix-pills
