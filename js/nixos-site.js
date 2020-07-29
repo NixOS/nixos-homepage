809148c5a7273ff21ba6a9d96cb2eaf57c552234
Rok Garbas <rok@garbas.si>
2020-07-29 12:24:52 +0200

    Searching for options from learn page now uses correct link

--- js/nixos-site.js
+++ js/nixos-site.js
@@ -3,14 +3,8 @@ $(document).ready(function () {
 
   $("#learn-options-search button, #options-search button").click(function (event) {
     event.preventDefault();
-    var url = "https://search.nixos.org";
-    if ($(this).hasClass("search-options")) {
-      url += "/options";
-      url += "?query=" + $("input", $(this).parents("form")).val()
-    } else {
-      url += "/packages"
-      url += "?query=" + $("input", $(this).parents("form")).val();
-    }
+    var url = "https://search.nixos.org/options";
+    url += "?query=" + $("input", $(this).parents("form")).val()
     window.location.href = url;
   });
 
