45f1c92be80775a8cbd9fb3d836794acffdebc9b
Rok Garbas <rok@garbas.si>
2020-09-03 14:07:30 +0200

    disable (temporary) updating of nix unstable flake

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ UPDATE=1 nix shell nixpkgs#gnumake nixpkgs#curl -c make update --keep-going
 nix flake update \
   --update-input released-nixpkgs-unstable \
   --update-input released-nixpkgs-stable \
-  --update-input released-nix-unstable \
   --update-input released-nix-stable \
   --update-input nix-pills
+#--update-input released-nix-unstable \
