4613f04a4a7dd7817074489e3a0c8aac24c28404
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-09-05 17:25:33 -0400

    Revert "DONTMERGEME: don't update flakes"
    This reverts commit 27ea2e2a358db3404c9076231c7c59ae64b13f31.

--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@ set -e
 
 UPDATE=1 nix shell nixpkgs#gnumake nixpkgs#curl -c make update --keep-going
 
-# nix flake update \
-#   --update-input released-nixpkgs-unstable \
-#   --update-input released-nixpkgs-stable \
-#   --update-input released-nix-unstable \
-#   --update-input released-nix-stable \
-#   --update-input nix-pills
+nix flake update \
+  --update-input released-nixpkgs-unstable \
+  --update-input released-nixpkgs-stable \
+  --update-input released-nix-unstable \
+  --update-input released-nix-stable \
+  --update-input nix-pills
