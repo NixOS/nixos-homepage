c446e5f8ead2302343460af0f766eb3c90aae1e4
Rok Garbas <rok@garbas.si>
2020-09-07 21:31:53 +0200

    run updates also for nix-dev input

--- update.sh
+++ update.sh
@@ -9,4 +9,5 @@ nix flake update \
   --update-input released-nixpkgs-stable \
   --update-input released-nix-unstable \
   --update-input released-nix-stable \
-  --update-input nix-pills
+  --update-input nix-pills \
+  --update-input nix-dev
