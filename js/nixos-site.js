f3defc58c02d8acf8b6614f2d372b109daf671b9
Rok Garbas <rok@garbas.si>
2020-09-17 16:58:44 +0200

    show asciinema-player more as part of the page

--- js/nixos-site.js
+++ js/nixos-site.js
@@ -69,7 +69,7 @@ $(function () {
         history.replaceState(null, null, " ");
       }
     });
-    $pane.append($el);
+    $("div", $pane).first().append($el);
   })
 
   // Make a whole element act as if the first link or button was clicked.
