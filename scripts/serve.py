c315af1874412780799ea65b1dfef7f1f639d994
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-16 13:55:17 -0500

    serve: common styles are already watched
    There is no need to explicitly call for watching common styles.

--- scripts/serve.py
+++ scripts/serve.py
@@ -4,18 +4,7 @@ import os
 
 
 @click.command()
-@click.option(
-    '--common-styles',
-    default=None,
-    type=click.Path(
-        exists=True,
-        file_okay=False,
-        dir_okay=True,
-        readable=True,
-        resolve_path=True,
-    ),
-)
-def main(common_styles):
+def main():
     server = livereload.Server()
 
     paths_to_watch = [
@@ -32,9 +21,6 @@ def main(common_styles):
         "./site-styles/**/*",
         "./site-styles/**/**/*",
     ]
-
-    if common_styles is not None:
-        paths_to_watch.append(common_styles)
 
     for path in paths_to_watch:
         server.watch(path, lambda: os.system("make"))
