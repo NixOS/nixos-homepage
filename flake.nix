db78bc2a63ab0d9cd2dd6ad571325638db1cbe79
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-04 00:39:27 +0100

    Migrate the stable nixpkgs input to nixos-22.11

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@ rec {
 
   # These inputs are used for the manuals and release artifacts
   inputs.released-nixpkgs-unstable = { url = "nixpkgs/nixos-unstable"; };
-  inputs.released-nixpkgs-stable = { url = "nixpkgs/release-22.11"; };
+  inputs.released-nixpkgs-stable = { url = "nixpkgs/nixos-22.11"; };
   inputs.released-nix-unstable = { url = "github:nixos/nix/master"; };
   inputs.released-nix-stable = { url = "github:nixos/nix/latest-release"; };
   inputs.nix-pills = { url = "github:NixOS/nix-pills"; flake = false; };
