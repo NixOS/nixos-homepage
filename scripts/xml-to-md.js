f9d7573c0881355874277b096ec45f1df24bb3fe
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-12-07 00:19:27 +0100

    fix small issue with the xml to md script

--- scripts/xml-to-md.js
+++ scripts/xml-to-md.js
@@ -11,13 +11,13 @@ function generateMDCOntent(collectionName) {
         const title = item.getElementsByTagName('title')[0].textContent.replace(/(\r\n|\n|\r)/gm, "").replace(/ +(?= )/g,'');
         const pubDate = new Date(item.getElementsByTagName('pubDate')[0].textContent)
         const content = new TurndownService().turndown(new XMLSerializer().serializeToString(item.getElementsByTagName('description')[0]))
-        writeFileSync(`src/contant/blog/${collectionName}/${pubDate.toISOString().substring(0,10)}_${id}.md`, `--
+        writeFileSync(`src/content/blog/${collectionName}/${pubDate.toISOString().substring(0,10)}_${id}.md`, `---
 id: ${id}
 title:${title}
 date: ${pubDate.toISOString()}
 category: ${collectionName}
 ---
-${content}`)
+${content}`, { flag: 'ax' })
     })
 }
 
