93a355391593faaa24783ef7b14ede1907ac83b2
Johannes Kirschbauer <hsjobeki@gmail.com>
2024-07-02 21:46:25 +0200

    Format: init prettier astro and ci checks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -6,21 +6,21 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 // theme derivated from https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/main/packages/tm-themes/themes/vesper.json
-import syntaxTheme from "./src/lib/shiki/theme.json"
+import syntaxTheme from "./src/lib/shiki/theme.json";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://nixos.org',
+  output: "static",
+  site: "https://nixos.org",
   integrations: [
     tailwind(),
     mdx(),
     sitemap(),
     icon({
       include: {
-        mdi: ['*'],
-        simpleIcons: ['*'],
-      }
+        mdi: ["*"],
+        simpleIcons: ["*"],
+      },
     }),
   ],
   markdown: {
