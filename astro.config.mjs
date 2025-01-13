98ddce0ba1f866a2fe2030b0d72952444e1e5e7b
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-01-13 22:33:02 +0100

    fix: generate device and browser specific favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,24 @@ import icon from "astro-icon";
 // theme derivated from https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/main/packages/tm-themes/themes/vesper.json
 import syntaxTheme from "./src/lib/shiki/theme.json";
 
+import favicons from "astro-favicons";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://nixos.org",
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap(),
-    icon({
-      include: {
-        mdi: ["*"],
-        simpleIcons: ["*"],
-      },
-    }),
-  ],
+  integrations: [tailwind(), mdx(), sitemap(), icon({
+    include: {
+      mdi: ["*"],
+      simpleIcons: ["*"],
+    },
+  }), favicons({
+    manifest: {
+      name: "Nix &amp; NixOS | Declarative builds and deployments",
+      short_name: "Nix &amp; NixOS",
+      start_url: "/",
+    }
+  })],
   legacy: {
     collections: true, // TODO: migrate to Content Layer API
   },
