dc09b2d4728c38ad686cd0d67a61fc0e60fea651
Will Fancher <elvishjerricco@gmail.com>
2025-03-17 21:04:59 -0400

    Revert "add unstable iso image download (#1677)"
    This reverts commit 15289f5b82c0dae276181023e486c88a436d41e4.

--- src/lib/links.js
+++ src/lib/links.js
@@ -11,7 +11,6 @@ export const nixosDownloadLink = (
   arch,
   format = 'iso',
   sha256 = false,
-  nixos_version = NIXOS_VERSION,
 ) => {
-  return `https://channels.nixos.org/nixos-${nixos_version}/latest-nixos-${variant ? `${variant}-` : ''}${arch}.${format}${sha256 ? '.sha256' : ''}`;
+  return `https://channels.nixos.org/nixos-${NIXOS_VERSION}/latest-nixos-${variant ? `${variant}-` : ''}${arch}.${format}${sha256 ? '.sha256' : ''}`;
 };
