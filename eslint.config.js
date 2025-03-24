92f5ad5f11b42bafb256a671fdfa81299c42a067
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-03-24 16:35:30 +0100

    add eslint and solve some basic linter problems

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: [
+    "*.{js,mjs,cjs,ts}",
+    "src/**/*.{js,mjs,cjs,ts}",
+  ], languageOptions: { globals: globals.node } },
+  { files: ["public/**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  [globalIgnores([
+    "public/bootstrap/**/*",
+    '.astro/**/*',
+  ])]
+]);
