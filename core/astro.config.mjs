4d2d591fc3ed5ed0e861e7c1152edd9ebb31ed44
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-04-05 17:25:35 +0200

    Draft: Add "Seasonal Themes" and "Custom Banners" (#1719)
    This PR adds the ability to have custom seasonal themes (mainly
    introduced for our rainbow logo for now) as well as custom banners that
    exceed the capabilities and design of our traditional announcement blog
    post banner.
    
    While both can be integrated into the codebase upfront, the activation
    will be handled via environment variables named `THEME` and `BANNER`.
    These can be introduced at build time to control the output and are
    designed to be activated or deactivated by the CI at specific time
    periods. This design allows us to work on these things asynchronously
    without interfering with other works on our codebase.

--- core/astro.config.mjs
+++ core/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -7,8 +7,8 @@ import icon from "astro-icon";
 
 // theme derivated from https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/main/packages/tm-themes/themes/vesper.json
 import syntaxTheme from "./src/lib/shiki/theme.json";
-
 import favicons from "astro-favicons";
+import { getNixLogoUrlUniversal } from "./src/lib/utils.js";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +20,11 @@ export default defineConfig({
       simpleIcons: ["*"],
     },
   }), favicons({
+    input: {
+      favicons: [
+        getNixLogoUrlUniversal(process.env.THEME, '.')
+      ],
+    },
     name: "Nix & NixOS",
     short_name: "Nix & NixOS",
     manifest: {
@@ -34,5 +39,11 @@ export default defineConfig({
   compressHTML: true,
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+  env: {
+    schema: {
+      THEME: envField.string({ context: "client", access: "public", default: "default" }),
+      BANNER: envField.string({ context: "client", access: "public", default: "default" }),
+    },
+  },
 });
