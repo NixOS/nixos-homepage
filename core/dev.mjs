a69aedd37255afe317d61ccfd5325ff712cbee68
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-04-08 21:55:52 +0200

    improve dev server experience

--- core/dev.mjs
+++ core/dev.mjs
@@ -1,5 +1,9 @@
 import { dev } from "astro";
 import express from 'express';
+import chalk from 'chalk';
+
+console.log(chalk.gray('--- Spinning up Astro dev server...'));
+console.log(chalk.red('--- The link outputted by the Astro dev server does not include external content like manuals, pills and demos'));
 
 const app = express();
 const devServer = await dev({
@@ -15,5 +19,12 @@ app.use('/demos', express.static(process.env.PATH_DEMOS))
 app.use(devServer.handle);
 
 app.listen(devServer.address.port + 1, () => {
-  console.log(`--- http://localhost:${devServer.address.port + 1} ---`)
+  console.log(
+    '\n' +
+    chalk.bgGreen.black('--- Dev env startup complete') +
+    '\n' +
+    chalk.gray('--- The following link does include external content like manuals, pills and demos\n') +
+    chalk.green(`--- Dev server running at `) +
+    chalk.underline.bold(`http://localhost:${devServer.address.port + 1}`)
+  );
 })
