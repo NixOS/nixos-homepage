c0e44c7107c7561ef045830d1c1457d12112f4b4
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-04-10 23:29:36 +0200

    Reintroduce nix build functionality (#1727)
    This PR reintroduces `nix build` functionality for the homepage and also
    serves external dependencies like manuals, pills etc. via a development
    server.

---
+++ core/devserver.mjs
@@ -0,0 +1,43 @@
+import { dev } from 'astro';
+import express from 'express';
+import chalk from 'chalk';
+
+console.log(chalk.gray('--- Spinning up Astro dev server...'));
+console.log(
+  chalk.red(
+    '--- The link outputted by the Astro dev server does not include external content like manuals, pills and demos',
+  ),
+);
+
+const app = express();
+const devServer = await dev({
+  root: './.',
+  server: {
+    port: 4320,
+  },
+  vite: {
+    server: {
+      hmr: {
+        clientPort: 4320,
+      },
+    },
+  },
+});
+
+app.use('/manual', express.static(process.env.PATH_MANUAL));
+app.use('/guides/nix-pills', express.static(process.env.PATH_PILLS));
+app.use('/demos', express.static(process.env.PATH_DEMOS));
+app.use(devServer.handle);
+
+app.listen(devServer.address.port + 1, () => {
+  console.log(
+    '\n' +
+      chalk.bgGreen.black('--- Dev env startup complete') +
+      '\n' +
+      chalk.gray(
+        '--- The following link does include external content like manuals, pills and demos\n',
+      ) +
+      chalk.green(`--- Dev server running at `) +
+      chalk.underline.bold(`http://localhost:${devServer.address.port + 1}`),
+  );
+});
