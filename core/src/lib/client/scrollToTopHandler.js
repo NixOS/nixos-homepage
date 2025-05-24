1cc1d5534aaaa838f1df78e735d2f6c93f13acb1
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-05-24 14:28:07 +0200

    make arrow stick to side

--- core/src/lib/client/scrollToTopHandler.js
+++ core/src/lib/client/scrollToTopHandler.js
@@ -5,7 +5,7 @@ document.addEventListener('scroll', () => {
   const scrollPosition = window.scrollY;
   const scrollPositionBottom = scrollPosition + window.innerHeight;
   if (scrollPositionBottom > document.body.offsetHeight - 10) {
-    scrollTop.classList.remove('bottom-4', 'right-4', 'bg-nix-blue-darker');
+    scrollTop.classList.remove('bottom-8', '-right-14', 'bg-nix-blue-darker');
     scrollTop.classList.add(
       'md:bottom-4',
       'right-1/2',
@@ -15,11 +15,11 @@ document.addEventListener('scroll', () => {
   } else {
     scrollTop.classList.remove(
       'md:bottom-4',
-      'right-20',
+      'right-1/2',
       'translate-x-1/2',
       'bottom-0',
     );
-    scrollTop.classList.add('bottom-4', 'right-4', 'bg-nix-blue-darker');
+    scrollTop.classList.add('bottom-8', '-right-14', 'bg-nix-blue-darker');
   }
 
   if (scrollPosition > 100) {
