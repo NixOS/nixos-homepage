0d71f317bf771b0327186a2848935119661aa951
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2026-02-23 00:51:49 +0100

    format

--- core/src/lib/takumi-og.tsx
+++ core/src/lib/takumi-og.tsx
@@ -1,27 +1,34 @@
-import type { RenderFunctionInput } from "astro-takumi";
+import type { RenderFunctionInput } from 'astro-takumi';
 import React from 'react';
 
-export async function nixOg({ title, description }: RenderFunctionInput): Promise<React.ReactNode> {
+export async function nixOg({
+  title,
+  description,
+}: RenderFunctionInput): Promise<React.ReactNode> {
   return Promise.resolve(
     <div
       style={{
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        alignItems: "start",
-        justifyContent: "center",
-        flexDirection: "column",
-        backgroundColor: "#4d6fb7",
-        textAlign: "center",
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        alignItems: 'start',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        backgroundColor: '#4d6fb7',
+        textAlign: 'center',
         padding: 32,
       }}
     >
-      <div style={{
-        color: "#ffffff",
-        fontSize: 72,
-        fontWeight: 900,
-        fontFamily: "Roboto Flex"
-      }}>{title}</div>
+      <div
+        style={{
+          color: '#ffffff',
+          fontSize: 72,
+          fontWeight: 900,
+          fontFamily: 'Roboto Flex',
+        }}
+      >
+        {title}
+      </div>
     </div>,
   );
 }
