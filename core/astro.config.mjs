23c3b8698dc152bde427d30df19a57057b909db7
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2026-02-23 00:51:24 +0100

    start implementing takumi for og image generation

--- core/astro.config.mjs
+++ core/astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig, envField } from 'astro/config';
 
+import fs from "node:fs";
+
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
@@ -11,6 +13,8 @@ import favicons from 'astro-favicons';
 
 import { createRequire } from 'module';
 import shellPromptTransformer from './src/lib/shiki/shellPromptTransformer';
+import takumi, { presets } from 'astro-takumi';
+import { nixOg } from './src/lib/takumi-og';
 const require = createRequire(import.meta.url);
 
 function selectFavicon(theme = 'default') {
@@ -25,26 +29,30 @@ function selectFavicon(theme = 'default') {
 export default defineConfig({
   output: 'static',
   site: 'https://nixos.org',
-  integrations: [
-    mdx(),
-    sitemap(),
-    icon({
-      include: {
-        mdi: ['*'],
-        simpleIcons: ['*'],
+  integrations: [mdx(), sitemap(), icon({
+    include: {
+      mdi: ['*'],
+      simpleIcons: ['*'],
+    },
+  }), favicons({
+    input: {
+      favicons: [selectFavicon(process.env.THEME)],
+    },
+    name: 'Nix & NixOS',
+    short_name: 'Nix & NixOS',
+    manifest: {
+      start_url: '/',
+    },
+  }), takumi(
+    {
+      options: {
+        fonts: [
+          fs.readFileSync("../node_modules/@fontsource-variable/roboto-flex/files/roboto-flex-latin-standard-normal.woff2"),
+        ],
       },
-    }),
-    favicons({
-      input: {
-        favicons: [selectFavicon(process.env.THEME)],
-      },
-      name: 'Nix & NixOS',
-      short_name: 'Nix & NixOS',
-      manifest: {
-        start_url: '/',
-      },
-    }),
-  ],
+      render: nixOg
+    }
+  )],
   markdown: {
     shikiConfig: {
       theme: syntaxTheme,
