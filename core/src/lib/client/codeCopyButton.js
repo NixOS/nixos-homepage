b2d6b6b0142e8d595f153fb12123588f47e74d9f
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2026-06-18 17:45:05 +0200

    add button text to multiline variant

--- core/src/lib/client/codeCopyButton.js
+++ core/src/lib/client/codeCopyButton.js
@@ -13,30 +13,40 @@ for (const pre of document.querySelectorAll('pre.astro-code')) {
 
   const copyButton = document.createElement('button');
 
-  const copyButtonText = document.createElement('span');
-  copyButtonText.classList.add(
-    'mt-1.5',
+  const copyButtonIcon = document.createElement('span');
+  copyButtonIcon.classList.add(
     'w-5',
     'h-5',
     'iconify',
     'icon-[mdi--content-copy]',
   );
+  copyButton.appendChild(copyButtonIcon);
+
+  const copyButtonText = document.createElement('span');
+  copyButtonText.classList.add();
   copyButton.appendChild(copyButtonText);
 
-  copyButton.classList.add(
-    'cursor-pointer',
-    'text-white',
-    'h-full',
-    'w-full',
-    'px-4',
-  );
+  copyButton.classList.add('cursor-pointer', 'text-white', 'h-full', 'px-4');
   header.appendChild(copyButton);
 
   header.classList.add('bg-secondary-afghani-blue-45', 'text-primary-white');
 
-  if (pre.querySelectorAll('.line').length > 1) {
+  const isMultiLine = pre.querySelectorAll('.line').length > 1;
+
+  if (isMultiLine) {
     pre.classList.add('rounded-b-none!', 'mb-0');
     wrapper.classList.add('flex-col', 'mb-4');
+
+    copyButton.classList.add(
+      'rounded-2xl',
+      'flex',
+      'items-center',
+      'justify-center',
+      'gap-2',
+    );
+
+    copyButtonText.innerText = 'Copy';
+    copyButtonText.classList.add('mt');
 
     header.classList.add(
       'flex',
@@ -51,6 +61,10 @@ for (const pre of document.querySelectorAll('pre.astro-code')) {
     );
   } else {
     pre.classList.add('rounded-r-none!');
+
+    copyButton.classList.add('rounded-r-2xl', 'w-full');
+
+    copyButtonIcon.classList.add('mt-1.5');
 
     header.classList.add('min-h-max', 'mt-2', 'mb-4', 'rounded-r-2xl');
   }
@@ -67,13 +81,21 @@ for (const pre of document.querySelectorAll('pre.astro-code')) {
 
     navigator.clipboard.writeText(code.innerText.trim()).then(() => {
       copyButton.classList.add('bg-accent-zambian-green-55!');
-      copyButton.classList.add('bg-secondary');
-      copyButtonText.classList.remove('icon-[mdi--content-copy]');
-      copyButtonText.classList.add('icon-[mdi--check]');
+      copyButtonIcon.classList.remove('icon-[mdi--content-copy]');
+      copyButtonIcon.classList.add('icon-[mdi--check]');
+
+      if (isMultiLine) {
+        copyButtonText.innerText = 'Copied!';
+      }
+
       setTimeout(() => {
         copyButton.classList.remove('bg-accent-zambian-green-55!');
-        copyButtonText.classList.add('icon-[mdi--content-copy]');
-        copyButtonText.classList.remove('icon-[mdi--check]');
+        copyButtonIcon.classList.add('icon-[mdi--content-copy]');
+        copyButtonIcon.classList.remove('icon-[mdi--check]');
+
+        if (isMultiLine) {
+          copyButtonText.innerText = 'Copy';
+        }
       }, 2000);
     });
   });
