f66e0b1885c57048cc8a4e23de77b975c3f0c390
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2026-06-19 14:44:32 +0200

    customize og image appearance

--- core/src/lib/takumi-og.tsx
+++ core/src/lib/takumi-og.tsx
@@ -1,5 +1,20 @@
 import type { RenderFunctionInput } from 'astro-takumi';
 import React from 'react';
+import path from 'path';
+import fs from 'fs';
+
+const filePath = path.join(
+  process.cwd(),
+  '..',
+  'node_modules',
+  '@nixos',
+  'branding',
+  'artifacts',
+  'internal',
+  'nixos-logo-white-flat-white-regular-horizontal-none.svg',
+);
+
+const imageBase64 = `data:image/png;base64,${fs.readFileSync(filePath).toString('base64')}`;
 
 export async function nixOg({
   title,
@@ -8,26 +23,49 @@ export async function nixOg({
   return Promise.resolve(
     <div
       style={{
-        display: 'flex',
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridTemplateRows: '1fr 1fr',
         height: '100%',
         width: '100%',
-        alignItems: 'start',
-        justifyContent: 'center',
-        flexDirection: 'column',
         backgroundColor: '#4d6fb7',
-        textAlign: 'center',
         padding: 32,
       }}
     >
       <div
         style={{
-          color: '#ffffff',
-          fontSize: 72,
-          fontWeight: 900,
-          fontFamily: 'Roboto Flex',
+          gridRow: '1 / 2',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'start',
         }}
       >
-        {title}
+        <img
+          src={imageBase64}
+          style={{
+            height: 140,
+            marginBottom: '2rem',
+          }}
+        />
+      </div>
+      <div
+        style={{
+          gridRow: '2 / 3',
+          display: 'flex',
+          alignItems: 'start',
+          justifyContent: 'start',
+        }}
+      >
+        <div
+          style={{
+            color: '#ffffff',
+            fontSize: 72,
+            fontWeight: 600,
+            fontFamily: 'Roboto Flex',
+          }}
+        >
+          {title}
+        </div>
       </div>
     </div>,
   );
